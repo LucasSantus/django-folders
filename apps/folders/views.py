1bfb267c9ee135468e9036ce5ff5fe6e8c4f5bf5
Lucas Santos <leos9877@gmail.com>
2022-02-16 13:40:48 -0300

    :rocket: adicionando listagem de sub pastas.

--- apps/folders/views.py
+++ apps/folders/views.py
@@ -8,9 +8,7 @@ def register_folder(request):
         form = RegisterFolderForm(request.POST)
         if form.is_valid():
             form.save()
-            
             messages.success(request, f"Registered sucess!")
-
             return redirect('/')
 
     context = {
@@ -20,3 +18,34 @@ def register_folder(request):
 
     return render(request, "folders/register_folder.html", context)
 
+def register_sub_folder(request, slug):
+    folder = Folder.objects.get(slug = slug)
+    form = RegisterFolderForm()
+    if request.method == "POST":
+        form = RegisterFolderForm(request.POST)
+        if form.is_valid():
+            # form.save(commit = False)
+            form.sub_folder = True
+            form.folder = folder
+            form.save()
+
+            messages.success(request, f"Registered sucess!")
+            return redirect('/')
+
+    context = {
+        'form': form,
+        'action': 'Register'
+    }
+
+    return render(request, "folders/register_folder.html", context)
+
+def list_sub_folders(request, slug_folder):
+    folder = Folder.objects.get(slug = slug_folder)
+    sub_folders = Folder.objects.filter(folder = folder)
+    
+    context = {
+        'folder': folder,
+        'sub_folders': sub_folders
+    }
+
+    return render(request, "folders/list_sub_folders.html", context)
