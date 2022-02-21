add7aba018422e966cd9401bd6e1f6b4464761ae
Lucas Santos <leos9877@gmail.com>
2022-02-21 17:07:50 -0300

    :rocket: adicionando django-autoslug

--- apps/folders/models.py
+++ apps/folders/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-from django.template.defaultfilters import slugify
+# from django.template.defaultfilters import slugify
+from autoslug import AutoSlugField
+
 class Folder(models.Model):
     title = models.CharField("title", max_length = 120)
     sub_folder = models.BooleanField(default = False, null = True, blank = True)
     folder = models.ForeignKey("self", on_delete = models.CASCADE, related_name = 'folder_FolderFK', null = True, blank = True)
-    slug = models.SlugField(unique = True, null = True, blank = True)
+    # slug = models.SlugField(unique = True, null = True, blank = True)
+    slug = AutoSlugField(populate_from = 'title', unique = True, editable = True)
     time_registered = models.DateTimeField("Horário Registrado", auto_now_add = True)
 
     class Meta:
@@ -15,7 +18,7 @@ class Folder(models.Model):
     def __str__(self):
         return self.title
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        return super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     if not self.slug:
+    #         self.slug = slugify(self.title)
+    #     return super().save(*args, **kwargs)
